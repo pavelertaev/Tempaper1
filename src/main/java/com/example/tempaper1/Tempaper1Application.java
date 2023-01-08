469ddc0bd0aa0d07cac9d121ab64f5a593166dfe
паша <pashka_ertaev@mail.ru>
2023-01-08 13:35:58 +0300

    Initial commit

---
+++ src/main/java/com/example/tempaper1/Tempaper1Application.java
@@ -0,0 +1,13 @@
+package com.example.tempaper1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tempaper1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tempaper1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tempaper1/Tempaper1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tempaper1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tempaper1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
